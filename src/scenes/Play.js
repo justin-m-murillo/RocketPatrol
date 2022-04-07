da2ef2f4f4acbdd94c06781740580cea10358ac1
Justin Murillo <murillo_justin@yahoo.com>
2022-04-06 20:19:13 -0700

    Added Spaceship.update() & 3 instances of Spaceship in Play

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -22,6 +22,10 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
         this.p1Rocket = new Rocket(this, game.config.width/2, game.config.height - borderUISize - borderPadding, 'rocket').setOrigin(0.5, 0);
+        // add spaceships (x3)
+        this.ship01 = new Spaceship(this, game.config.width + borderUISize*6, borderUISize*4, 'spaceship', 0, 30).setOrigin(0, 0);
+        this.ship02 = new Spaceship(this, game.config.width + borderUISize*3, borderUISize*5 + borderPadding*2, 'spaceship', 0, 20).setOrigin(0, 0);
+        this.ship03 = new Spaceship(this, game.config.width, borderUISize*6 + borderPadding*4, 'spaceship', 0, 10).setOrigin(0, 0);
         
         // define keys
         keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
@@ -31,7 +35,10 @@ class Play extends Phaser.Scene {
     }
 
     update() {
-        this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
+        this.starfield.tilePositionX -= 4; // moving starfield backdrop
+        this.p1Rocket.update(); // update rocket
+        this.ship01.update(); // update spaceship (x3)
+        this.ship02.update();
+        this.ship03.update();
     }
 }
